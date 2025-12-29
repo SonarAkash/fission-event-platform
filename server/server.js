15036eef762b4d870c93f25cba03f5948ea77069
Akash <akashsonar.9113@gmail.com>
2025-12-29 14:43:07 +0530

    singup & login Api complete

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
 
 //  env vars
 dotenv.config();
@@ -14,6 +15,8 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json()); 
+
+app.use('/api/auth', authRoutes);
 
 
 app.get('/', (req, res) => {
